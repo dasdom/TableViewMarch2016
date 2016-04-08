13e72122fe3efdf1507df29fa669fea645dece0b
Dominik Hauser <dom@dasdom.de>
2016-04-08 13:50:05 +0200

    Make the favorit way the default

--- TableViewMarch2016/AppDelegate.swift
+++ TableViewMarch2016/AppDelegate.swift
@@ -15,8 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
-//    window?.rootViewController = UINavigationController(rootViewController: UserSearchTableViewController<User>())
-    window?.rootViewController = UINavigationController(rootViewController: TraditionalUserSerachTableViewController())
+    window?.rootViewController = UINavigationController(rootViewController: UserSearchTableViewController<User>())
+//    window?.rootViewController = UINavigationController(rootViewController: TraditionalUserSerachTableViewController())
     window?.makeKeyAndVisible()
     return true
   }
